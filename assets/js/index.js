efff52e01f15f9573d572106ee2e73a4104c84da
Christian Camilo Beltran Fonseca <cambelf.dev@gmail.com>
2025-06-14 00:21:03 -0500

    Add firts progress in gallery build

---
+++ assets/js/index.js
@@ -0,0 +1,130 @@
+const API_KEY = "MJxQU2QSeSbw78R8ToBiJ5IH011yJsO43QdzMQ3wkq7FFyspIUCVKH9J";
+const API_URL_IMAGES = "https://api.pexels.com/v1/";
+const API_URL_VIDEOS = "with https://api.pexels.com/videos/";
+const GALLERY_CONTAINER = document.querySelector("#js-images-container");
+
+function createLightbox() {}
+
+
+
+
+// Function to fetch images by category from Pexels API
+async function getImagesByCategory(category){
+
+    let fetchURL = `${API_URL_IMAGES}search?query=${category}&per_page=20`;
+
+    try {
+
+        const response = await fetch(fetchURL, {
+            headers: {
+                "Authorization": API_KEY,
+            }
+        });
+
+        const jsonResponse = await response.json();
+        let photosCollection = jsonResponse.photos;
+        console.log("Photos Collection:", photosCollection);
+        
+        createImagesCard(photosCollection)
+
+    } catch (error) {
+        console.error("Error fetching images:", error);
+    }
+}
+
+
+function createImagesCard(images){
+    images.forEach( image => {
+        const imageCard =`
+            <div class="image">
+                <a href="${image.src.large}" data-lightbox="gallery" >
+                    <figure>
+                        <img src="${image.src.medium}" alt="${image.alt}" loading="lazy">
+                    </figure>
+                </a>
+
+                <div class="image-info">
+                    <h2>Photo by: <a href="${image.photographer_url}"> ${image.photographer} on Pexels</a></h2>
+                </div>
+            </div>
+        `;
+        
+        GALLERY_CONTAINER.innerHTML += imageCard;
+    })
+
+    activateLightbox();
+
+}
+
+
+function createLightbox(imageUrl, imageAlt) {
+    let overlay = document.createElement("div");
+    overlay.classList.add("lightbox-overlay");
+    document.body.appendChild(overlay);
+
+    let imageContainer = document.createElement("div");
+    imageContainer.classList.add("lightbox-image-container");
+    overlay.appendChild(imageContainer);
+
+    let image = document.createElement("img");
+    image.src = imageUrl;
+    image.alt = imageAlt;
+
+    let closer = document.createElement("span");
+    closer.classList.add("lightbox-close");
+    closer.innerHTML = "&times;";
+
+    let loader = document.createElement("div");
+    loader.classList.add("loader");
+    for( let i = 0; i < 12; i++) {
+        let bar = document.createElement("div");
+        bar.classList.add(`bar${i+1}`);
+        loader.append(bar);
+    }
+
+    imageContainer.append(closer);
+    imageContainer.append(image);
+    imageContainer.append(loader);
+    overlay.append(imageContainer);
+
+
+    image.onload = () => {
+        loader.style.display = "none";
+        image.style.opacity = "1";
+        closer.style.display = "grid";
+    }
+
+    
+
+    closer.addEventListener("click", () => {
+        document.body.removeChild(overlay);
+    });
+
+    overlay.addEventListener("click", (e) => {
+        if (e.target === overlay) {
+            document.body.removeChild(overlay);
+        }
+    });
+}
+
+
+function activateLightbox() {
+    if(document.querySelectorAll("a[data-lightbox]")){
+        const lightboxButtons = document.querySelectorAll("a[data-lightbox]");
+        lightboxButtons.forEach(button => {
+            button.addEventListener("click", (e) => {
+                e.preventDefault();
+                const imageUrl = button.getAttribute("href");
+                const imageAlt = button.getAttribute("alt");
+                createLightbox(imageUrl, imageAlt);
+            });
+        });
+    }
+}
+
+
+
+
+
+getImagesByCategory("colombia");
+
